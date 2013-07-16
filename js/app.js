896c4f8c3a4992e04797621688ffab2872d43328
jblanchard <jblanchard@jblanchard-ltm.internal.salesforce.com>
2013-07-16 09:51:41 -0500

    Fix Case Obj Factory

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ angular.module('Account', []).factory('Account', function (AngularForceObjectFac
 });
 
 angular.module('Case', []).factory('Case', function (AngularForceObjectFactory) {
-    var Case = AngularForceObjectFactory({type: 'Case'});
+    var Case = AngularForceObjectFactory({type: 'Case', fields: ['Id', 'CaseNumber', 'Contact.Name', 'Subject', 'Status'], where: '', limit: 50});
     return Case;
 });
 
